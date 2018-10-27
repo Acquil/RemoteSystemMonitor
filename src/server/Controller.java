3903a5626d6343953c7d854ddbf4a6b1c2eaf526
Acquil <acquil98@gmail.com>
2018-10-27 15:55:30 +0530

    Added proper GUI

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -1,4 +1,31 @@
 package server;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Controller {
+
+    @FXML
+    Label lblSetIP;
+    @FXML
+    Label lblSetPort;
+
+    @FXML
+    private void initialize() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        lblSetIP.setText(address.getHostAddress());
+        lblSetPort.setText("15001");
+    }
+
+
+
 }
--- src/server/Main.java
+++ src/server/Main.java
@@ -1,24 +1,87 @@
 package server;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.concurrent.Task;
 
 public class Main extends Application {
+
+    private Integer port = 15001;
+
+    private Task connAccepter = new Task<Void>() {
+        @Override
+        public Void call() throws Exception {
+            ServerSocket socket = null;
+            try {
+                socket = new ServerSocket(port);
+                while(true){
+                    Socket client = socket.accept();
+                    System.out.println("New client connected :" + socket.toString());
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    };
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("RSM - Server");
 
+//        Label lblPort = (Label)root.lookup("#lblSetPort");
+//        lblPort.setText("15001");
+
         primaryStage.setScene(new Scene(root, 720, 520));
+
+        new Thread(connAccepter).start();
+
         primaryStage.show();
+
+
+        //To close threads on exit
+        primaryStage.setOnCloseRequest(windowEvent -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
     }
 
 
-    public static void main(String[] args) {
+
+
+
+    public static void main(String[] args){
+
+//        Runnable connAccepter = new AcceptIncomingConn(15001);
+//        Thread incomingConn = new Thread(connAccepter);
+//        incomingConn.setDaemon(true);
+//        incomingConn.start();
+
         launch(args);
+
+
+//        incomingConn.join();
+
     }
 }
